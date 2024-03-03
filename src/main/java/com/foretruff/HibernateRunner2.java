510784a02c62ac0b10a30586fcdc9a18d400a48f
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-03-03 19:55:47 +0200

    lesson 18

--- src/main/java/com/foretruff/HibernateRunner2.java
+++ src/main/java/com/foretruff/HibernateRunner2.java
@@ -2,34 +2,34 @@ package com.foretruff;
 
 import com.foretruff.entity.User;
 import com.foretruff.util.HibernateUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class HibernateRunner2 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HibernateRunner2.class);
+//    private static final Logger log = LoggerFactory.getLogger(HibernateRunner2.class);
 
     public static void main(String[] args) throws SQLException {
         User user1 = User.builder()
-                .username("Ivan818")
+                .username("Ivan81118")
                 .firstname("Ivan")
                 .lastname("Ivanov")
                 .build();
 
-        LOGGER.info("User entity is in transient state , object: {}", user1);
+        log.info("User entity is in transient state , object: {}", user1);
 
         try (var sessionFactory = HibernateUtil.buildSessionFactory()) {
             var session1 = sessionFactory.openSession();
             try (session1) {
                 var transaction = session1.beginTransaction();
-                LOGGER.trace("User is in persistent state: {} , session: {}", user1, transaction);
+                log.trace("User is in persistent state: {} , session: {}", user1, transaction);
 
                 session1.persist(user1);
 
                 transaction.commit();
             }
-            LOGGER.warn("User is in detached state: {} , session is closed {}", user1, session1);
+            log.warn("User is in detached state: {} , session is closed {}", user1, session1);
 //            try (var session2 = sessionFactory.openSession()) {
 //                session2.beginTransaction();
 //
@@ -43,7 +43,7 @@ public class HibernateRunner2 {
 //                session2.getTransaction().commit();
 //            }
         } catch (Exception exception) {
-            LOGGER.error("Exception occurred", exception);
+            log.error("Exception occurred", exception);
             throw exception;
         }
     }
